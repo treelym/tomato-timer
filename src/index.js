7d81db69222fc96ee54f559ffaf04a7d375b5921
treelym <65914904+treelym@users.noreply.github.com>
2023-02-08 01:01:19 -0500

    Sound the alarm!

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ function timeNode() {
   return document.getElementById('time');
 }
 
+function displayTimeLeft(minutes = 25, seconds = 0) {
+  timeNode().innerText =
+    `${minutes.toString().padEnd(2, 0)}:${seconds.toString().padStart(2, 0)}`;
+}
+
+function stopTimer() {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+}
+
+function soundTheAlarm() {
+  const alarm = new Audio('./most-annoying-sound-in-the-world.mp3');
+  alarm.play();
+}
+
 function tick() {
   const timeLeft = timeNode().innerText;
   const [minutesLeft, secondsLeft] = timeLeft.split(':').map(parseFloat);
   if (minutesLeft === 0 && secondsLeft === 0) {
     stopTimer();
-    // TODO: Sound the alarm
+    soundTheAlarm();
   } else if (secondsLeft === 0) {
     displayTimeLeft(minutesLeft - 1, 59);
   } else {
@@ -21,20 +37,9 @@ function startTimer() {
   intervalId = setInterval(tick, 1000);
 }
 
-function stopTimer() {
-  if (intervalId) {
-    clearInterval(intervalId);
-  }
-}
-
 function resetTimer() {
   stopTimer();
   displayTimeLeft();
-}
-
-function displayTimeLeft(minutes = 25, seconds = 0) {
-  timeNode().innerText =
-    `${minutes.toString().padEnd(2, 0)}:${seconds.toString().padStart(2, 0)}`;
 }
 
 displayTimeLeft();
