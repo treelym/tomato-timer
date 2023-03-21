f27ce050e4923eb0dcb28deed47515980cc5a22e
treelym <65914904+treelym@users.noreply.github.com>
2023-03-21 12:01:58 -0400

    Implement animated timer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'bulma/css/bulma.min.css';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <section className='section is-medium has-background-black'>
+        <Component {...pageProps} />
+      </section>
+    </>
+  );
 }
