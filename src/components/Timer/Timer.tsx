598d59e6cb79cddcdbdf5f818ec7eeecc2567aee
treelym <65914904+treelym@users.noreply.github.com>
2023-03-30 11:24:11 -0400

    Refactor Buttons into their own component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -5,40 +5,34 @@ interface TimerProps {
   secondsLeft?: number;
 }
 
-interface IconProps {
-  pathShape: string;
-  testId: string;
+enum ButtonColors {
+  BLACK = 'black',
+  DANGER = 'danger',
+  DARK = 'dark',
+  GHOST = 'ghost',
+  INFO = 'info',
+  LIGHT = 'light',
+  LINK = 'link',
+  PRIMARY = 'primary',
+  SUCCESS = 'success',
+  TEXT = 'text',
+  WARNING = 'warning',
+  WHITE = 'white'
 }
 
-const Icon = ({ pathShape, testId }: IconProps): JSX.Element => {
-  const fillColor = '#3498db';
-  return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='72' height='72' data-testid={testId}>
-      <path fill='none' d='M0 0h24v24H0z' />
-      <path fill={fillColor} d={pathShape} />
-    </svg>
-  );
-};
+interface ButtonProps {
+  color: ButtonColors;
+  onClick: () => void;
+  text: string;
+}
 
-const PlayIcon = (): JSX.Element => (
-  <Icon
-    pathShape='M19.376 12.416L8.777 19.482A.5.5 0 0 1 8 19.066V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0 1 0 .832z'
-    testId='play-button'
-  />
-);
-
-const PauseIcon = (): JSX.Element => (
-  <Icon
-    pathShape='M6 5h2v14H6V5zm10 0h2v14h-2V5z'
-    testId='play-pause-button'
-  />
-);
-
-const ResetIcon = (): JSX.Element => (
-  <Icon
-    pathShape='M18.537 19.567A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10c0 2.136-.67 4.116-1.81 5.74L17 12h3a8 8 0 1 0-2.46 5.772l.997 1.795z'
-    testId='reset-button'
-  />
+const Button = ({ color, onClick, text }: ButtonProps): JSX.Element => (
+  <button
+    className={`button is-${color} is-outlined is-large is-fullwidth`}
+    onClick={onClick}
+  >
+    {text}
+  </button>
 );
 
 const Timer = ({ minutesLeft = 25, secondsLeft = 0 }: TimerProps): JSX.Element => {
@@ -104,12 +98,17 @@ const Timer = ({ minutesLeft = 25, secondsLeft = 0 }: TimerProps): JSX.Element =
             <div className='timer-text'>
               <h2 className='timer-heading has-text-light has-text-weight-semibold'>{formatTime(minutes, seconds)}</h2>
               <div className='timer-controls'>
-                <span className='timer-control timer-play-pause' onClick={intervalId ? handleStopTimer : handleStartTimer}>
-                  {intervalId ? <PauseIcon /> : <PlayIcon />}
-                </span>
-                <span className='timer-control timer-reset ml-2' onClick={handleResetTimer}>
-                  <ResetIcon />
-                </span>
+                <div className='columns'>
+                  <div className='column'>
+                    <Button color={ButtonColors.SUCCESS} onClick={handleStartTimer} text='Start' />
+                  </div>
+                  <div className='column'>
+                    <Button color={ButtonColors.DANGER} onClick={handleStopTimer} text='Stop' />
+                  </div>
+                  <div className='column'>
+                    <Button color={ButtonColors.INFO} onClick={handleResetTimer} text='Reset' />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
